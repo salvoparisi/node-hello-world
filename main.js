6ba80d567f86d648bd97746aa16eb8cc6b861cb5
salvoparisi <psalvo065@gmail.com>
2024-10-28 14:34:27 +0100

    add argument and bonus

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 console.log("hello node js");
 
 const pin = process.env.PIN
-
 console.log(pin);
+
+let argument = process.argv
+argument.push(pin)
+console.log(argument);
+
+if(pin==666){
+    console.log("Welcome Admin");
+}else{
+    console.log("Access restricted");
+}
 
